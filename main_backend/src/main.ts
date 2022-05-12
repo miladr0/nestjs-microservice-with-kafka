a84b60480d43aee4fc256528130cb9fa9c8fa68d
miladr0 <miladr0r@gmail.com>
2022-05-12 18:29:30 +0430

    run main backend as microservice and api at the same time

--- main_backend/src/main.ts
+++ main_backend/src/main.ts
@@ -1,12 +1,25 @@
 import { NestFactory } from '@nestjs/core';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.connectMicroservice({
+    transport: Transport.RMQ,
+    options: {
+      urls: ['amqp://127.0.0.1:5672'],
+      queue: 'main_queue',
+      queueOptions: {
+        durable: false,
+      },
+    },
+  });
   app.setGlobalPrefix('api');
   app.enableCors({
     origin: '*',
   });
+
+  await app.startAllMicroservices();
   await app.listen(8001);
 }
 bootstrap();
