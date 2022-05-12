eb9ec48f2806d163e0ed23248eef8effc1627fbb
miladr0 <miladr0r@gmail.com>
2022-05-12 18:30:08 +0430

    send queue messages from admin to main through RabbitMQ

--- admin_backend/src/product/product.module.ts
+++ admin_backend/src/product/product.module.ts
@@ -1,11 +1,28 @@
 import { Module } from '@nestjs/common';
+import { ClientsModule, Transport } from '@nestjs/microservices';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { MAIN_PRODUCT_QUEUE } from 'src/constants/constants';
 import { ProductController } from './product.controller';
 import { Product } from './product.entinty';
 import { ProductService } from './product.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Product])],
+  imports: [
+    TypeOrmModule.forFeature([Product]),
+    ClientsModule.register([
+      {
+        name: MAIN_PRODUCT_QUEUE,
+        transport: Transport.RMQ,
+        options: {
+          urls: ['amqp://localhost:5672'],
+          queue: 'main_queue',
+          queueOptions: {
+            durable: false,
+          },
+        },
+      },
+    ]),
+  ],
   controllers: [ProductController],
   providers: [ProductService],
 })
